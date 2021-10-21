4278495941a8e9b872b4dc086825bf37c317838b
Ekaterina Mochalova <emochalova@sfedu.ru>
2021-10-21 09:23:19 +0300

    Находит вичокосный ли год

---
+++ IsLeapYear.pas
@@ -0,0 +1,8 @@
+function IsLeapYear(year: integer): boolean;
+begin
+  
+  if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
+    result := True;
+  else
+    result := False;
+end;
